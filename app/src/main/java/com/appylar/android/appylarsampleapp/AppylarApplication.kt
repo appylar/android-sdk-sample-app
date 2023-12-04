71920aec8b786912d895e739acb531ee5399f83c
Daniel Winther <daniel.kirgiopoulos@gmail.com>
2023-12-04 22:10:19 +0100

    chore: initial public release

---
+++ app/src/main/java/com/appylar/android/appylarsampleapp/AppylarApplication.kt
@@ -0,0 +1,37 @@
+package com.appylar.android.appylarsampleapp
+
+import android.app.Application
+import android.util.Log
+import com.appylar.android.sdk.Appylar
+import com.appylar.android.sdk.enums.AdType
+import com.appylar.android.sdk.enums.Orientation
+import com.appylar.android.sdk.interfaces.Events
+
+class AppylarApplication : Application(), Events {
+    private val tag = "AppylarSampleApp"
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Set event listener before initialization
+        Appylar.setEventListener(this)
+
+        // Initialize
+        Appylar.init(
+            this, // The application context
+            "<appKey>", // The unique app key for your app
+            arrayOf(AdType.BANNER, AdType.INTERSTITIAL), // The ad types that you want to show
+            true // Test mode, true for development, false for production
+        )
+    }
+
+    // Event listener triggered if an error occurs in the SDK
+    override fun onError(error: String) {
+        Log.d(tag, "onError(): $error")
+    }
+
+    // Event listener triggered at successful initialization
+    override fun onInitialized() {
+        Log.d(tag, "onInitialized()")
+    }
+}
---
+++ app/src/main/java/com/appylar/android/appylarsampleapp/KotlinActivity.kt
@@ -0,0 +1,76 @@
+package com.appylar.android.appylarsampleapp
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.appylar.android.sdk.bannerview.BannerView
+import com.appylar.android.sdk.bannerview.BannerViewListener
+import com.appylar.android.sdk.interstitial.Interstitial
+import com.appylar.android.sdk.interstitial.InterstitialListener
+
+class KotlinActivity : AppCompatActivity(), BannerViewListener, InterstitialListener {
+    private val tag = "AppylarSampleApp"
+    private lateinit var bannerView: BannerView
+    private lateinit var btnShowBanner: Button
+    private lateinit var btnHideBanner: Button
+    private lateinit var btnShowInterstitial: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bannerView = findViewById(R.id.bannerView)
+        btnShowBanner = findViewById(R.id.btnShowBanner)
+        btnHideBanner = findViewById(R.id.btnHideBanner)
+        btnShowInterstitial = findViewById(R.id.btnShowInterstitial)
+
+        // Set event listener for the banner view
+        bannerView.setEventListener(this)
+
+        // Attach banner button click listener
+        btnShowBanner.setOnClickListener {
+            if (bannerView.canShowAd()) {
+                bannerView.showAd()
+            }
+        }
+        btnHideBanner.setOnClickListener {
+            bannerView.hideAd()
+        }
+
+        // Set event listener for the interstitial
+        Interstitial.setEventListener(this) // Attach interstitial event listeners
+
+        // Attach interstitial button click listener
+        btnShowInterstitial.setOnClickListener {
+            if (Interstitial.canShowAd()) {
+                Interstitial.showAd(this)
+            }
+        }
+    }
+
+    // Event listener triggered when a banner is shown for the parameter height
+    override fun onBannerShown(height: Int) {
+        Log.d(tag, "onBannerShown($height)")
+    }
+
+    // Event listener triggered when there are no banners to show
+    override fun onNoBanner() {
+        Log.d(tag, "onNoBanner()")
+    }
+
+    // Event listener triggered when an interstitial is shown
+    override fun onInterstitialShown() {
+        Log.d(tag, "onInterstitialShown()")
+    }
+
+    // Event listener triggered when an interstitial is closed
+    override fun onInterstitialClosed() {
+        Log.d(tag, "onInterstitialClosed()")
+    }
+
+    // Event listener triggered when there are no interstitials to show
+    override fun onNoInterstitial() {
+        Log.d(tag, "onNoInterstitial()")
+    }
+}
